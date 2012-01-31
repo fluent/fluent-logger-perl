0bf0a7fc79a3195e721394fcd1c72bc93e8097d5
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2012-01-31 12:53:54 +0900

    v0.04

--- lib/Fluent/Logger.pm
+++ lib/Fluent/Logger.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use Mouse;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use IO::Socket::INET;
 use IO::Socket::UNIX;
@@ -377,6 +377,8 @@ FUJIWARA Shunichiro E<lt>fujiwara _at_ cpan.orgE<gt>
 Kazuki Ohta
 
 FURUHASHI Sadayuki
+
+lestrrat
 
 =head1 REPOSITORY
 
